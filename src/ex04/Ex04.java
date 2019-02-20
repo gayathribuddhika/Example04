db0fd122b1cbe1e9668b6a02d097e93ead87d9d2
gayathribuddhika <39160904+gayathribuddhika@users.noreply.github.com>
2019-02-20 16:45:45 +0530

    Example04
    User Inputs

---
+++ src/ex04/Ex04.java
@@ -0,0 +1,21 @@
+
+package ex04;
+
+import java.util.Scanner;
+public class Ex04 {
+
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        String s = "Welcome to HackerRank's Java tutorials!";
+        System.out.println(s);
+        System.out.println(" ");
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+    
+}
